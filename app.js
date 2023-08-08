6ce1669d2e3fbe7bc5b1e9512e443c2d42dc8d98
JuanRumelfanger <umajuanrumel@gmail.com>
2023-08-08 20:36:18 -0300

    Creo el puero por app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app= express();
+const path= require("path");
+const publicPtah = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPtah)); 
+
+//Levantar el servidor
+
+const port= process.env.PORT || 3001;
+app.listen(port, ()=>console.log("Servidor corriendo en puerto "+port  ));
+
+app.get("/home",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./viewes/home.html"))
+})

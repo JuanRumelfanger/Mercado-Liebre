fe54fe56a58c110a501228edcab4e4f734929564
JuanRumelfanger <umajuanrumel@gmail.com>
2023-08-14 13:08:21 -0300

    Cambio el archivo app.js

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(express.static(publicPtah));
 const port= process.env.PORT || 3001;
 app.listen(port, ()=>console.log("Servidor corriendo en puerto "+port  ));
 
-app.get("/home",(req,res)=>{
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "views/home.html"))
 })

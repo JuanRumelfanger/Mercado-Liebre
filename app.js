8e996f07ecb833b8cb203cad919e0d73249dd91b
JuanRumelfanger <umajuanrumel@gmail.com>
2023-08-14 14:33:37 -0300

    Soluciono directorios

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app= express();
 const path= require("path");
 const publicPtah = path.resolve(__dirname, 'public');
 
+const viewsPtah = path.resolve(__dirname, 'views');
+
 app.use(express.static(publicPtah)); 
+
+app.use(express.static(viewsPtah)); 
 
 //Levantar el servidor
 
@@ -12,10 +16,4 @@ app.listen(port, ()=>console.log("Servidor corriendo en puerto "+port  ));
 
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname, "views/home.html"))
-})
-app.get("/register",(req,res)=>{
-    res.sendFile(path.resolve(__dirname, "register.html"))
-})
-app.get("/login",(req,res)=>{
-    res.sendFile(path.resolve(__dirname, "login.html"))
 })

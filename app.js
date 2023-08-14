199323fad90b8890f229f7be56bd229396702f41
JuanRumelfanger <umajuanrumel@gmail.com>
2023-08-14 14:26:21 -0300

    Soluciono directorios

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname, "views/home.html"))
 })
 app.get("/register",(req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/register.html"))
+    res.sendFile(path.resolve(__dirname, "register.html"))
 })
 app.get("/login",(req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/login.html"))
+    res.sendFile(path.resolve(__dirname, "login.html"))
 })
